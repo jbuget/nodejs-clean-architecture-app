28d6e1867fd3ffbf7fa820663f9f1b82b1518203
Jérémy Buget <jbuget@gmail.com>
2018-01-05 16:39:01 +0100

    Extract and move Hapi server into a dedicated file into 'frameworks & drivers' layer

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 'use strict';
 
-const Hapi = require('hapi');
-const Blipp = require('blipp');
-
 // Create a server with a host and port
-const server = Hapi.server({
-  port: process.env.PORT || 3000
-});
-
-// Add the route
-server.route(require('./lib/interface_adapters/routes'));
+const createServer = require('./lib/technical_drivers/server');
 
 // Start the server
 const start = async () => {
 
-  await server.register(Blipp);
-
   try {
+    const server = await createServer();
     await server.start();
+
+    console.log('Server running at:', server.info.uri);
   }
   catch (err) {
     console.log(err);
     process.exit(1);
   }
-
-  console.log('Server running at:', server.info.uri);
 };
 
 start();
