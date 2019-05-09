b14aa968c21c85ee9b830acede3bbd28950d483c
Jérémy Buget <jbuget@gmail.com>
2019-05-09 02:13:58 +0200

    Add an example of SQL/ORM repository implem.
    - Add Sequelize ORM
    - Add SQLIte3 DB driver
    - Define the DB Sequelize model "user"
    - Implement UserRepositorySQLite and call it in UsersController
    (commented)

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 'use strict';
 
 // Create a server with a host and port
+const sequelize = require('./lib/infrastructure/database/sequelize');
 const createServer = require('./lib/infrastructure/webserver/server');
 
 // Start the server
 const start = async () => {
 
   try {
+
+    await sequelize.sync();
+    console.log('Connection to DB has been established successfully.');
+
+  } catch (err) {
+    console.error('Unable to connect to the database:', err);
+  }
+
+  try {
     const server = await createServer();
     await server.start();
 
     console.log('Server running at:', server.info.uri);
-  }
-  catch (err) {
+  } catch (err) {
     console.log(err);
     process.exit(1);
   }
