b975668fb967efb99f726808c3422cfa1e05b67d
Kenneth Acquah <kkacquah@mit.edu>
2019-12-25 13:40:56 -0500

    GetDatapoints test completed with sample data

--- test/application_business_rules/use_cases/GetDatapoints.test.js
+++ test/application_business_rules/use_cases/GetDatapoints.test.js
@@ -1,24 +1,17 @@
 const User = require('../../../lib/enterprise_business_rules/entities/User');
-const DatapointsRepository = require('../../../lib/application_business_rules/repositories/UserRepository');
-const mockDatapointsRepository = new DatapointsRepository();
-const getDatapoints = require('../../../lib/application_business_rules/use_cases/getDatapoints');
+const DatapointsRepository = require('../../../lib/application_business_rules/repositories/DatapointsRepository');
+const getDatapoints = require('../../application_business_rules/use_cases/getDatapoints');
+const testQuery = require('../../data/searchQueryResolverTestQuery.js');
+const testDatapoints = require('../../data/searchQueryResolverTestQuery.js');
 
 test('Should resolve query to get a datapoints object with history mapping to query request', async () => {
   // given
-  const expectedDatapoints = {
-
-  }
-  const query = {
-    phrases: ["Neural Networks", "Bayesian Networks","Markov Methods", "Evolutionary Algorithms", "Support Vector Machine"],
-    interval: {
-      start: 682128000,
-      end: 1577222874
-    }
-  }
+  const testQuery = require('../../data/searchQueryResolverTestQuery.js');
+  const testDatapoints = require('../../data/searchQueryResolverTestQuery.js');
+  mockUserRepository.resolve = jest.fn(() => testDatapoints);
   // when
-  const datapoints = await getDatapoints(query, { userRepository: mockDatapointsRepository });
-
+  const datapoints = await GetDatapoints(testQuery, { datapointsRepository: mockDatapointsRepository });
   // then
-  expect(mockDatapointsRepository.resolve).toHaveBeenCalledWith(new User(null, 'John', 'Doe', 'john.doe@email.com', 'P@s$W0rD'));
-  expect(datapoints).toEqual(expectedDatapoints);
+  expect(mockDatapointsRepository.resolve).toHaveBeenCalledWith(testQuery);
+  expect(datapoints).toEqual(testDatapoints);
 });
