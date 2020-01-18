10dbd31dedda85d519a3467e3567a81513ef561b
Kenneth Acquah <kkacquah@mit.edu>
2020-01-18 17:41:02 -0500

    set params in session now!

--- test/data/searchQueryResolverTestQuery.js
+++ test/data/searchQueryResolverTestQuery.js
@@ -1,9 +1,8 @@
 module.exports = {
   //List of phrases search for t generate the visualization.
-  phrases: ["Neural Networks", "Bayesian Networks", "Markov Methods", "Evolutionary Algorithms", "Support Vector Machine"],
-  interval: { //Interval of the start and end time of the query in milliseconds since epoch.
-    start: 682128000,
-    end: 1577222874
-  },
+  phrase: "Neural Networks",
+  //Interval of the start and end time of the query in milliseconds since epoch.
+  start: 682128000,
+  end: 1577222874,
   category: "cs" //Category as represented by ArXiv short code.
 }
