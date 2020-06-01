db547e349809ff369ccc4282c7786abf595e4aab
jbuget <jbuget@gmail.com>
2020-06-01 16:06:57 +0200

    Add environment configuration

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 'use strict';
 
-// Create a server with a host and port
-const sequelize = require('./lib/infrastructure/orm/sequelize/sequelize');
+const bootstrap = require('./lib/infrastructure/config/bootstrap');
 const createServer = require('./lib/infrastructure/webserver/server');
 
 // Start the server
 const start = async () => {
 
-/*
   try {
-    await sequelize.sync();
-    console.log('Connection to DB has been established successfully.');
-  } catch (err) {
-    console.error('Unable to connect to the database:', err);
-  }
-*/
+    await bootstrap.init();
 
-  try {
     const server = await createServer();
     await server.start();
 
